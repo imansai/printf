7b0fb165e7a24769c21a6c7bb7fe76002774e9e7
HanRachid <93451709+HanRachid@users.noreply.github.com>
2023-04-24 19:09:08 +0000

    Update _printf.c

--- _printf.c
+++ _printf.c
@@ -30,7 +30,8 @@ int _printf(const char *format, ...)
 
 			else if (format[i + 1] == 'c')
 				charcount += get_fm_func(mod)(ptrap);
-
+			else if (format[i + 1] == '%')
+				_putchar('%');
 			i++;
 		}
 		else
