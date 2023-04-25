310664a6514073420c3b1ac641ac6540c68304cc
imansai <im.saibari@gmail.com>
2023-04-25 16:29:30 -0700

    add task 2

--- main.h
+++ main.h
@@ -1,15 +1,15 @@
-#ifndef MAIN_H
 #define MAIN_H
 
 #include <stdarg.h>
 int _putchar(char c);
 int _printf(const char *format, ...);
-int print2(char c);
+int handlepercent(char c);
 int fm_char(va_list *ptr);
 int fm_str(va_list *ptr);
 int (*get_fm_func(char s))(va_list *ptr);
 int fm_int(va_list(*ptr));
 int _putnbr(int nb);
+int fm_bin(va_list *ptr);
 
 /**
  * struct fm - Struct fm
