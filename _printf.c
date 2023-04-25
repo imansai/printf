d80bc8e717241e2ddd2b120474cbf4ca673acada
imansai <im.saibari@gmail.com>
2023-04-25 16:16:19 -0700

    add task 2

--- _printf.c
+++ _printf.c
@@ -1,7 +1,60 @@
-#include "main.h"
 #include <stdarg.h>
 #include <stdio.h>
+#include <limits.h>
 
+/**
+ * handlepercent - Prints % and char
+ * @c: char to print
+ * Return: number of characters printed
+ */
+int handlepercent(char c)
+{
+	if (c != '%')
+	{
+		_putchar('%');
+		_putchar(c);
+		return (2);
+	}
+	if (c == '\0')
+		return (0);
+	_putchar('%');
+	return (1);
+}
+
+/**
+ * _putnbr - Prints number
+ * @num: number to print
+ * Return: int count
+ */
+
+int _putnbr(int num)
+{
+	int count = 0;
+
+	if (num == INT_MIN)
+	{
+		_putchar('-');
+		_putchar('2');
+		num = 147483648;
+		count += 2;
+	}
+	else if (num < 0)
+	{
+		_putchar('-');
+		num = -num;
+		count++;
+	}
+
+	if (num > 9)
+	{
+		count += _putnbr(num / 10);
+	}
+
+	_putchar(num % 10 + '0');
+	count++;
+
+	return (count);
+}
 /**
  * _printf - Prints string with formating
  * @format: pointer to str
@@ -26,14 +79,14 @@ int _printf(const char *format, ...)
 			mod = format[i + 1];
 			while (mod == ' ')
 				mod = format[i++ + 2];
-			if (mod == 's' || mod == 'c' || mod == 'i' || mod == 'd')
+			if (mod == 's' || mod == 'c' || mod == 'i' || mod == 'd' || mod == 'b')
 				charcount += get_fm_func(mod)(ptrap);
 			else if (mod == '%')
 			{
-				charcount += print2('%');
+				charcount += handlepercent('%');
 			}
 			else if (format[i + 1] != '\0')
-				charcount += print2(mod);
+				charcount += handlepercent(mod);
 			i++;
 		}
 		else if (format[i] != '%')
